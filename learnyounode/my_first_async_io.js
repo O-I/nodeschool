f486940883341419e8e7b6fda2e0ccbaa98d8e38
Rahul Horé <hore.rahul@gmail.com>
2014-01-23 17:15:12 -0500

    Adds official solution

--- learnyounode/my_first_async_io.js
+++ learnyounode/my_first_async_io.js
@@ -3,3 +3,13 @@ fs.readFile(process.argv[2], 'utf-8', function newline_count(error, file_content
     if (error) throw error;
     console.log(file_contents.split('\n').length - 1);
 });
+
+// Official solution:
+// var fs = require('fs')
+// var file = process.argv[2]
+
+// fs.readFile(file, function (err, contents) {
+//   // fs.readFile(file, 'utf8', callback) can also be used
+//   var lines = contents.toString().split('\n').length - 1
+//   console.log(lines)
+// })
