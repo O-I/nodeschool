62bca06e0c556b67ad427c09ebdb9915da24970b
Rahul Horé <hore.rahul@gmail.com>
2014-01-23 18:08:14 -0500

    Refactors to take input from command line

--- learnyounode/filtered_ls.js
+++ learnyounode/filtered_ls.js
@@ -1,12 +1,12 @@
-var fs = require('fs');
-var path = require('path');
-var pathname = '../learnyounode';
-var file_extension_filter = 'js';
+var fs = require('fs'),
+    path = require('path'),
+    pathname = process.argv[2],
+    file_extension_filter = process.argv[3];
 
 fs.readdir(pathname, function filtered_ls(error, files_list) {
   if (error) throw error;
-  //console.log(files_list.map(path.extname));
-  for (var index = 0; index < files_list.length; ++index)
+  var index = 0, length = files_list.length;
+  for (index; index < length; ++index)
     if (path.extname(files_list[index]) === '.' + file_extension_filter)
       console.log(files_list[index]);
 });
