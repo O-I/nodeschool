1725f478c49eb28c6fdbaaa621e3eae927f82442
Rahul Horé <hore.rahul@gmail.com>
2014-01-23 18:09:06 -0500

    Adds official solution

--- learnyounode/filtered_ls.js
+++ learnyounode/filtered_ls.js
@@ -10,3 +10,14 @@ fs.readdir(pathname, function filtered_ls(error, files_list) {
     if (path.extname(files_list[index]) === '.' + file_extension_filter)
       console.log(files_list[index]);
 });
+
+// Official solution:
+// var fs = require('fs')
+// var path = require('path')
+
+// fs.readdir(process.argv[2], function (err, list) {
+//   list.forEach(function (file) {
+//     if (path.extname(file) === '.' + process.argv[3])
+//       console.log(file)
+//   })
+// })
