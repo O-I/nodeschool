dbf7dcea203c155742bdf00206750219ca45a01b
Rahul Horé <hore.rahul@gmail.com>
2014-01-23 14:44:47 -0500

    Verifies my solution passes, adds 'official' solution, slightly refactors my own

--- baby_steps.js
+++ baby_steps.js
@@ -3,7 +3,15 @@ var index = 2,
     args = process.argv,
     length = args.length,
     total = 0;
-for (index; index < length; index++) {
+for (index; index < length; index++)
   total += Number(args[index]);
-}
 console.log(total);
+
+// Official solution:
+//
+// var result = 0
+//    
+// for (var i = 2; i < process.argv.length; i++)
+//   result += Number(process.argv[i])
+//
+// console.log(result)
