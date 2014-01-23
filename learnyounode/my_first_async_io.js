99c2527c57d6bcdc31ca2d28944a56560a20fac6
Rahul Horé <hore.rahul@gmail.com>
2014-01-23 17:14:14 -0500

    Switches to work on command line argument

--- learnyounode/my_first_async_io.js
+++ learnyounode/my_first_async_io.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-fs.readFile('test.txt', 'utf-8', function newline_count(error, file_contents) {
+fs.readFile(process.argv[2], 'utf-8', function newline_count(error, file_contents) {
     if (error) throw error;
     console.log(file_contents.split('\n').length - 1);
 });
