ce8eb33ad311ce062468babac8820391901448f0
Rahul Horé <hore.rahul@gmail.com>
2014-01-24 13:58:43 -0500

    Adds official solution

--- learnyounode/http_client.js
+++ learnyounode/http_client.js
@@ -7,3 +7,12 @@ http.get(url, function(response) {
     console.log(chunk);
   });
 });
+
+// Official solution:
+// var http = require('http')
+    
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// })
