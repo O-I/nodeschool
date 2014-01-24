00c7e1b7106bb47997f58fcf7f47f58e46e2f742
Rahul Horé <hore.rahul@gmail.com>
2014-01-24 13:01:38 -0500

    Updates main.js to take arguments from command line

--- learnyounode/make_it_modular/main.js
+++ learnyounode/make_it_modular/main.js
@@ -2,7 +2,7 @@ var files = require('./filtered_ls'),
     pathname = process.argv[2],
     file_extension_filter = process.argv[3];
 
-files.ls_filter('../../learnyounode', 'js', function(error, files_list) {
+files.ls_filter(pathname, file_extension_filter, function(error, files_list) {
   if (error) throw error;
   files_list.forEach(function(file) {
     console.log(file);
