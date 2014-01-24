b8d506892e9e1129c6b3d394eeee194621087f17
Rahul Horé <hore.rahul@gmail.com>
2014-01-24 13:00:13 -0500

    Removes all console.logs

--- learnyounode/make_it_modular/filtered_ls.js
+++ learnyounode/make_it_modular/filtered_ls.js
@@ -3,17 +3,12 @@ var path = require('path');
 var filtered_file_list = [];
 
 exports.ls_filter = function(pathname, file_extension_filter, callback) {
-  // console.log(pathname);
-  // console.log(file_extension_filter);
   fs.readdir(pathname, function(error, files_list) {
     if (error) return callback(error);
-    // console.log(files_list);
     files_list.forEach(function(file) {
-      // console.log(file);
       if (path.extname(file) === '.' + file_extension_filter)
         filtered_file_list.push(file);
     });
-    // return callback(null, filtered_file_list);
-    console.log(filtered_file_list);
+    return callback(null, filtered_file_list);
   });
 }
