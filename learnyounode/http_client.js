7b81f18203a0d7aa9dd6d67731cfa975b1012fa7
Rahul Horé <hore.rahul@gmail.com>
2014-01-24 13:57:34 -0500

    Verifies http_client.js passes specs

--- learnyounode/http_client.js
+++ learnyounode/http_client.js
@@ -1,9 +1,7 @@
 var http = require('http'),
     url = process.argv[2];
-    url = 'http://hi.o-i.im'
 
 http.get(url, function(response) {
-  console.log("Response code: " + response.statusCode);
   response.setEncoding('utf-8');
   response.on('data', function(chunk) {
     console.log(chunk);
