16f9d3b90f27c21003f8d011aff93f48cf453069
Rahul Horé <hore.rahul@gmail.com>
2014-01-24 13:07:28 -0500

    Switches to module.exports syntax to make it pass

--- learnyounode/make_it_modular/filtered_ls.js
+++ learnyounode/make_it_modular/filtered_ls.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var filtered_file_list = [];
 
-exports.ls_filter = function(pathname, file_extension_filter, callback) {
+module.exports = function(pathname, file_extension_filter, callback) {
   fs.readdir(pathname, function(error, files_list) {
     if (error) return callback(error);
     files_list.forEach(function(file) {
--- learnyounode/make_it_modular/main.js
+++ learnyounode/make_it_modular/main.js
@@ -1,8 +1,8 @@
-var files = require('./filtered_ls'),
+var ls_filter = require('./filtered_ls'),
     pathname = process.argv[2],
     file_extension_filter = process.argv[3];
 
-files.ls_filter(pathname, file_extension_filter, function(error, files_list) {
+ls_filter(pathname, file_extension_filter, function(error, files_list) {
   if (error) throw error;
   files_list.forEach(function(file) {
     console.log(file);
