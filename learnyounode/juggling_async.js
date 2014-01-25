99472216fb3a8ef87f01bb50a4e7a9036d4f6050
Rahul Horé <hore.rahul@gmail.com>
2014-01-25 18:29:07 -0500

    Passes JUGGLING ASYNC challenge

--- learnyounode/juggling_async.js
+++ learnyounode/juggling_async.js
@@ -2,26 +2,43 @@ var http = require('http'),
     bl = require('bl'),
     url_list = process.argv.slice(2, 5),
     length = url_list.length,
-    count = 0,
+    responses_received = 0,
     index = 0,
-    i = 0,
-    url = '',
-    text = [];
+    responses = [];
 
-for (i; i < length; i++) {
-  (function(index) {
-    url = url_list[index];
-    console.log(url);
-    http.get(url, function(response) {
-    //   console.log(response);
-      response.pipe(bl(function(error, data) {
-        if (error) return error;
-        console.log(data.toString());
-        text[index] = data.toString();
-        count++;
-        if (count === length)
-          text.forEach(console.log);
-      }));
+var httpGet = function(index, url) {
+  http.get(url, function(response) {
+    responses[index] = '';
+    response.pipe(bl(function(error, data) {
+      if (error) return error;
+      responses[index] += data.toString();
+    }));
+    response.on('end', function() {
+      responses_received++;
+      if (responses_received === length)
+        for (index; index < length; index++)
+          console.log(responses[index]);
     });
-  }(i));
+  });
 }
+
+for (index; index < length; index++)
+  httpGet(index, url_list[index]);
+
+// for (i; i < length; i++) {
+//   (function(index) {
+//     url = url_list[index];
+//     // console.log(url);
+//     http.get(url, function(response) {
+//     //   console.log(response);
+//       response.pipe(bl(function(error, data) {
+//         if (error) return error;
+//         // console.log(data.toString());
+//         text[index] = data.toString();
+//         count++;
+//         if (count === length)
+//           text.forEach(console.log);
+//       }));
+//     });
+//   }(i));
+// }
