4629d55b6b9db725150ec360bf7985b5ca2906fa
Rahul Horé <hore.rahul@gmail.com>
2014-01-25 18:32:43 -0500

    Adds official solution

--- learnyounode/juggling_async.js
+++ learnyounode/juggling_async.js
@@ -25,20 +25,31 @@ var httpGet = function(index, url) {
 for (index; index < length; index++)
   httpGet(index, url_list[index]);
 
-// for (i; i < length; i++) {
-//   (function(index) {
-//     url = url_list[index];
-//     // console.log(url);
-//     http.get(url, function(response) {
-//     //   console.log(response);
-//       response.pipe(bl(function(error, data) {
-//         if (error) return error;
-//         // console.log(data.toString());
-//         text[index] = data.toString();
-//         count++;
-//         if (count === length)
-//           text.forEach(console.log);
-//       }));
-//     });
-//   }(i));
+// Official solution:
+// var http = require('http')
+// var bl = require('bl')
+// var results = []
+// var count = 0
+
+// function printResults () {
+//   for (var i = 0; i < 3; i++)
+//     console.log(results[i])
 // }
+
+// function httpGet (index) {
+//   http.get(process.argv[2 + index], function (response) {
+//     response.pipe(bl(function (err, data) {
+//       if (err)
+//         return console.error(data)
+
+//       results[index] = data.toString()
+//       count++
+
+//       if (count == 3) // yay! we are the last one!
+//         printResults()
+//     }))
+//   })
+// }
+    
+// for (var i = 0; i < 3; i++)
+//   httpGet(i)
