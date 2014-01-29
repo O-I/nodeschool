a19602614a746860c291065aa24a783bd8176986
Rahul Horé <hore.rahul@gmail.com>
2014-01-29 12:34:05 -0500

    Removes unnecessary returns; troubleshooting lack of CAPS

--- stream_adventure/transform.js
+++ stream_adventure/transform.js
@@ -1,10 +1,10 @@
 var through = require('through');
 var tr = through(write, end);
 var write = function(buffer) {
-    return this.queue(buffer.toString().toUpperCase());
+    this.queue(buffer.toString().toUpperCase());
   };
 var end = function() {
-    return this.queue(null);
+    this.queue(null);
   };
 
 process.stdin.pipe(tr).pipe(process.stdout);
