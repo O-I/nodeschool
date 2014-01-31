56ff484d32060ae2b7e6443d8311669935abb00b
Rahul Horé <hore.rahul@gmail.com>
2014-01-31 18:13:34 -0500

    Adds official solution

--- stream_adventure/http_server.js
+++ stream_adventure/http_server.js
@@ -19,3 +19,17 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen(port);
+
+// Official solution
+// var http = require('http');
+// var through = require('through');
+
+// var server = http.createServer(function (req, res) {
+//     if (req.method === 'POST') {
+//         req.pipe(through(function (buf) {
+//             this.queue(buf.toString().toUpperCase());
+//         })).pipe(res);
+//     }
+//     else res.end('send me a POST\n');
+// });
+// server.listen(parseInt(process.argv[2]));
