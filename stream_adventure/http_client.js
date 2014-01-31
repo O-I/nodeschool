87fef45e2ed172ca73ace514c76b469e3e68de13
Rahul Horé <hore.rahul@gmail.com>
2014-01-31 18:55:55 -0500

    Adds official solution

--- stream_adventure/http_client.js
+++ stream_adventure/http_client.js
@@ -3,3 +3,8 @@ var request = require('request'),
     req = request.post(url);
 
 process.stdin.pipe(req).pipe(process.stdout);
+
+// Official solution:
+// var request = require('request');
+// var r = request.post('http://localhost:8000');
+// process.stdin.pipe(r).pipe(process.stdout);
