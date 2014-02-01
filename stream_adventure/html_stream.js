cd603803e215a6e76e709f8dc3e639a4496474e5
Rahul Horé <hore.rahul@gmail.com>
2014-02-01 18:38:28 -0500

    Adds official solution

--- stream_adventure/html_stream.js
+++ stream_adventure/html_stream.js
@@ -12,3 +12,15 @@ var embiggen = tr.select('.loud').createStream();
 embiggen.pipe(thru).pipe(embiggen);
 
 process.stdin.pipe(tr).pipe(process.stdout);
+
+// Official solution
+// var trumpet = require('trumpet');
+// var through = require('through');
+// var tr = trumpet();
+
+// var loud = tr.select('.loud').createStream();
+// loud.pipe(through(function (buf) {
+//     this.queue(buf.toString().toUpperCase());
+// })).pipe(loud);
+
+// process.stdin.pipe(tr).pipe(process.stdout);
