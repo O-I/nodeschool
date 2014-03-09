f77839cfc95b751ee526462785d289e32e675d8b
Rahul Horé <hore.rahul@gmail.com>
2014-03-08 21:10:25 -0500

    Adds official solution

--- stream_adventure/duplexer.js
+++ stream_adventure/duplexer.js
@@ -5,3 +5,12 @@ module.exports = function(command, arguments) {
   spawn = child_process(command, arguments);
   return duplex(spawn.stdin, spawn.stdout);
 };
+
+//Official solution
+// var spawn = require('child_process').spawn;
+// var duplexer = require('duplexer');
+
+// module.exports = function (cmd, args) {
+//   var ps = spawn(cmd, args);
+//   return duplexer(ps.stdin, ps.stdout);
+// };
