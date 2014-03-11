4b4d1bf530e905e80676ae8f36cb6d5002175c43
Rahul Horé <hore.rahul@gmail.com>
2014-03-10 21:17:15 -0400

    Adds official solution

--- stream_adventure/duplexer_redux.js
+++ stream_adventure/duplexer_redux.js
@@ -12,3 +12,18 @@ module.exports = function(counter) {
   });
   return duplex(tr, counter)
 };
+
+//Official solution
+// var duplexer = require('duplexer');
+// var through = require('through');
+
+// module.exports = function (counter) {
+//     var counts = {};
+//     var input = through(write, end);
+//     return duplexer(input, counter);
+    
+//     function write (row) {
+//         counts[row.country] = (counts[row.country] || 0) + 1;
+//     }
+//     function end () { counter.setCounts(counts) }
+// };
